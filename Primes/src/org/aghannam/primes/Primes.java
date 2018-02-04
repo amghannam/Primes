dd44579055de4fdef8d06390e428400cd3b64427
aghan <aghan@192.168.1.2>
2018-02-05 00:10:34 +0300

    Initial checkin.

---
+++ Primes/src/org/aghannam/primes/Primes.java
@@ -0,0 +1,227 @@
+/*
+ * File: Primes.java
+ */
+package org.aghannam.primes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * This class implements some common prime number operations, including a fairly
+ * efficient prime number generator.
+ * 
+ * @author Ahmed Ghannam
+ *
+ */
+public class Primes {
+	/**
+	 * For random number generation.
+	 */
+	private static Random rnd = ThreadLocalRandom.current();
+
+	private Primes() {
+		// Suppressed to prevent instantiability.
+	}
+
+	/**
+	 * Returns the nth prime number.
+	 * 
+	 * @param n
+	 *            the 'index' of the desired prime number
+	 * @return the nth prime number
+	 */
+	public static int nthPrime(int n) {
+		if (n == 1)
+			return 2;
+		if (n == 2)
+			return 3;
+		int candidate = 5;
+		int step = 4;
+		for (int count = 2; count < n; step = 6 - step, candidate += step) {
+			if (isPrime(candidate))
+				count++;
+		}
+		return candidate - step;
+	}
+
+	/**
+	 * Returns the number of primes in the specified list.
+	 * 
+	 * @param a
+	 *            the list containing the numbers
+	 * @return the number of primes in this list
+	 */
+	public static int count(int[] a) {
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (isPrime(a[i]))
+				count++;
+		}
+		return count;
+	}
+
+	/**
+	 * Returns the number of primes smaller than, but not including, the specified
+	 * value <b>n</b>.
+	 * 
+	 * @param n
+	 *            the value smaller than which to count primes
+	 * @return the number of primes smaller than n
+	 */
+	public static int countSmallerThan(int n) {
+		int count = 0;
+		for (int i = 2; i < n; i++) {
+			if (isPrime(i))
+				count++;
+		}
+		return count;
+	}
+
+	/**
+	 * Returns {@code true} if and only if the specified value <b>n</b> is a prime
+	 * number.
+	 * 
+	 * @param n
+	 *            the value to test for primality
+	 * @return {@code true} if <b>n</b> is a prime, {@code false} otherwise
+	 */
+	public static boolean isPrime(int n) {
+		if (n < 2)
+			return false;
+		if (n == 2 || n == 3)
+			return true;
+		if (n % 2 == 0 || n % 3 == 0)
+			return false;
+		int sqrtN = (int) Math.sqrt(n) + 1;
+		for (long i = 6L; i <= sqrtN; i += 6) {
+			if (n % (i - 1) == 0 || n % (i + 1) == 0)
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Returns a random prime number.
+	 * 
+	 * @return a random prime
+	 */
+	public static int random() {
+		int[] a = Generator.upTo(Integer.MAX_VALUE);
+		return a[rnd.nextInt(a.length)];
+
+	}
+
+	/**
+	 * Returns a random prime number up to, but not including, the specified bound.
+	 * 
+	 * @param bound
+	 *            the upper bound for the generated value (exclusive)
+	 * @return a random prime up to the specified bound
+	 */
+	public static int random(int bound) {
+		int[] a = Generator.upTo(bound);
+		return a[rnd.nextInt(a.length)];
+	}
+
+	/**
+	 * Returns a random prime number in the specified range.
+	 * 
+	 * @param lower
+	 *            the lower bound (inclusive, if prime)
+	 * @param upper
+	 *            the upper bound (exclusive)
+	 * @return a random prime in the specified range
+	 */
+	public static int random(int lower, int upper) {
+		int[] a = Generator.between(lower, upper);
+		return a[rnd.nextInt(a.length)];
+	}
+
+	public static class Generator {
+		/**
+		 * Current prime candidate used for sequential generation by method
+		 * {@code next()}.
+		 */
+		private static int candidate = 2;
+
+		/**
+		 * Returns an array of the first <b>n</b> prime numbers.
+		 * 
+		 * @param n
+		 *            the number of primes to generate
+		 * @return an array of the first <b>n</b> primes
+		 */
+		public static int[] firstN(int n) {
+			int candidate = 2;
+			int[] a = new int[n];
+			int i = 0;
+			while (i < n) {
+				if (isPrime(candidate)) {
+					a[i++] = candidate++;
+				} else {
+					candidate++;
+				}
+			}
+			return a;
+		}
+
+		/**
+		 * Returns an array containing all prime numbers up to, but not including, the
+		 * specified value <b>n</b>.
+		 * 
+		 * @param n
+		 *            the upper bound of the primes to be generated
+		 * @return an array containing all primes up to <b>n</b>
+		 */
+		public static int[] upTo(int n) {
+			int[] a = new int[countSmallerThan(n)];
+			return firstN(a.length);
+		}
+
+		/**
+		 * Returns an array containing all prime numbers between the specified
+		 * <b>lower</b> and <b>upper</b> bounds.
+		 * 
+		 * @param lower
+		 *            the lower bound (inclusive, if prime)
+		 * @param upper
+		 *            the upper bound (exclusive)
+		 * @return an array of all primes between lower and upper
+		 */
+		public static int[] between(int lower, int upper) {
+			if (lower == 2 && upper > lower)
+				return firstN(countSmallerThan(upper));
+			List<Integer> primes = new ArrayList<>();
+			for (int i = lower; i < upper; i++) {
+				if (isPrime(i))
+					primes.add(i);
+			}
+			return primes.stream().mapToInt(i -> i).toArray();
+		}
+
+		/**
+		 * Returns the next prime number in this generator's natural-ordered sequence.
+		 * <p>
+		 * To restart the generator, use {@code resetSequence()}.
+		 * 
+		 * @return the next prime in this generator's sequence, in natural order
+		 */
+		public static int next() {
+			while (!isPrime(candidate)) {
+				candidate++;
+			}
+			return candidate++;
+		}
+
+		/**
+		 * Resets the sequence of this generator such that a subsequent call to
+		 * {@code} next() starts from 2. (This does not affect the behavior of the other
+		 * methods.)
+		 */
+		public static void resetSequence() {
+			candidate = 2;
+		}
+	}
+}
---
+++ Primes/src/org/aghannam/primes/tests/PrimesTest.java
@@ -0,0 +1,66 @@
+package org.aghannam.primes.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.aghannam.primes.Primes;
+import org.junit.jupiter.api.Test;
+
+class PrimesTest {
+	int[] a = {2, 3, 5, 7, 11, 13, 17, 19}; 
+
+	@Test
+	void testNthPrime() {
+		assertEquals(2, Primes.nthPrime(1));
+		assertEquals(13, Primes.nthPrime(6)); 
+		assertEquals(7919, Primes.nthPrime(1000)); 
+	}
+
+	@Test
+	void testIsPrime() {
+		assertTrue(Primes.isPrime(2)); 
+		assertFalse(Primes.isPrime(4)); 
+		assertTrue(Primes.isPrime(7919)); 
+	}
+
+	@Test
+	void testCount() {
+		assertEquals(a.length, Primes.count(a)); 
+	}
+
+	@Test
+	void testCountSmallerThan() {
+		assertEquals(2, Primes.countSmallerThan(5)); 
+		assertEquals(a.length - 1, Primes.countSmallerThan(19)); 
+	}
+
+	@Test
+	void testFirstN() {
+		int[] b = Primes.Generator.firstN(a.length); 
+		assertArrayEquals(a, b); 
+	}
+
+	@Test
+	void testUpTo() {
+		int[] b = Primes.Generator.upTo(20); 
+		assertArrayEquals(a, b); 
+	}
+
+	@Test
+	void testBetween() {
+		int[] b = {5, 7}; 
+		int[] c = Primes.Generator.between(4, 11); 
+		assertArrayEquals(b, c); 
+	}
+	
+	@Test
+	void testNext() {
+		for (int i = 0; i < a.length; i++) {
+			assertEquals(a[i], Primes.Generator.next()); 
+		}
+		Primes.Generator.resetSequence(); 
+		for (int i = 0; i < a.length; i++) {
+			assertEquals(a[i], Primes.Generator.next()); 
+		}
+	}
+
+}
